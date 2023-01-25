1caa811c129696a9bcaefe3b2d3e47e1fb4d1269
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-01-26 00:39:19 +0200

    refactor: create window browserWindowFactory

---
+++ mainProcess/browserWindowFactory.js
@@ -0,0 +1,35 @@
+const {BrowserWindow} = require('electron');
+const {join} = require('path');
+
+const MAIN_WINDOW_WIDTH = 600;
+const MAIN_WINDOW_HEIGHT = 350;
+const LOADING_WINDOW_WIDTH = 300;
+const LOADING_WINDOW_HEIGHT = 200;
+
+
+module.exports = function windowFactory (windowType, parent) { /* Types such as: main, loading, alert */
+    let browserWindow = new BrowserWindow({
+        backgroundColor: '#343A40',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        resizable: false,
+        show: false,
+    });
+
+    if(windowType === 'main'){
+        browserWindow.setSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+        browserWindow.loadFile(join(__dirname, '../windows/mainWindow/mainWindow.html'));
+    } else if(windowType === 'loading'){
+        browserWindow.setSize(LOADING_WINDOW_WIDTH, LOADING_WINDOW_HEIGHT);
+        browserWindow.loadFile(join(__dirname, '../windows/loading window/loadingWindow.html'));
+    }
+
+    browserWindow.on('ready-to-show', () => {
+        browserWindow.center();
+        browserWindow.show();
+    });
+
+    return browserWindow;
+}
--- mainProcess/main.js
+++ mainProcess/main.js
@@ -1,31 +1,7 @@
-const {app, BrowserWindow, Menu, shell} = require('electron');
-const {join} = require('path');
-let mainWindow;
+const {app, Menu, shell} = require('electron');
+const windowFactory = require('./browserWindowFactory');
+let mainWindow, loadingWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 350,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-        resizable: false,
-        show: false,
-    });
-    mainWindow.on('ready-to-show', ()=> {
-        mainWindow.show();
-    })
-    mainWindow.loadFile(join(__dirname, '../windows/mainWindow/mainWindow.html'));
-   
-    mainWindow.webContents.on('will-navigate', (event, url) => {
-        console.log(url);
-        console.log('new window event');
-        event.preventDefault();
-        shell.openExternal(url);
-    })
-
-    // Menu.setApplicationMenu(Menu.buildFromTemplate([]));
+    mainWindow = windowFactory('main');
 });
-
-
