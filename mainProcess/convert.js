1c7e5270fabcf3f43c34d36373c94e0d754c2ca5
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-01-29 12:02:28 +0200

    fix: handle errors using reject instead of throw

--- mainProcess/convert.js
+++ mainProcess/convert.js
@@ -1,20 +1,30 @@
 const ffmpeg = require('fluent-ffmpeg');
 const { parse, join } = require('path');
 
-module.exports = function converter(source, saveFolderPath, targetExtension, loadingWindow) {
-	const {name, ext} = parse(source);   
+module.exports = function converter(
+	source,
+	saveFolderPath,
+	targetExtension,
+	loadingWindow
+) {
+	const { name, ext } = parse(source);
 	return new Promise((resolve, reject) => {
 		ffmpeg()
 			.input(source)
-			.output( join(saveFolderPath[0], `${name}.${targetExtension}`) )
-            .on('start', () => {
-                loadingWindow.webContents.send('start', `${name}${ext}`)
-            })
+			.output(join(saveFolderPath[0], `${name}.${targetExtension}`))
+			.on('start', () => {
+				loadingWindow.webContents.send('start', `${name}${ext}`);
+			})
 			.on('progress', ({ percent }) => {
-				loadingWindow.webContents.send('progress', Math.trunc(percent));
+				if (percent > 1) {
+					loadingWindow.webContents.send(
+						'progress',
+						Math.trunc(percent)
+					);
+				}
 			})
 			.on('error', (err) => {
-				throw new Error(err.message);
+				reject(err);
 			})
 			.on('end', () => {
 				resolve('done');
